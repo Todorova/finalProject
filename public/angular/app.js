1754e23bc88a0b56ba9b6cd481276af194ae2454
Ana Todorova <todorova@outlook.com>
2018-04-26 11:47:16 +0300

    add news categories and news btn in nav

--- public/angular/app.js
+++ public/angular/app.js
@@ -2,14 +2,14 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider
+        .when('/news', {
+            templateUrl: 'angular/news/news.htm',
+            controller: 'NewsController'
+        })
         .when('/video', {
             templateUrl: 'angular/video/video.htm',
             // controller: 'ChehliController'
         })
-        // .when('/user', {
-        //     templateUrl: 'js/user/user.htm',
-        //     controller: 'UserController'
-        // })
         .otherwise({
             templateUrl: 'angular/main/main.htm',
             controller: 'MainController'
--- public/angular/main/MainController.js
+++ public/angular/main/MainController.js
@@ -1,17 +1,43 @@
 app.controller('MainController', function ($scope, $http, $timeout) {
-    $http.get(window.location.origin+'/news').then(function (response) {
+
+    $http.get(window.location.origin + '/news/all').then(function (response) {
+        $scope.allNews = response.data;
+    });
+
+    $http.get(window.location.origin + '/news/latest').then(function (response) {
         $scope.latestNews = response.data;
     });
-    $scope.selected = 0;
 
+    $http.get(window.location.origin + '/news/top').then(function (res) {
+        $scope.topNews = res.data;
+    });
+    $http.get(window.location.origin + '/news/visited').then(function (res) {
+        $scope.visitedNews = res.data;
+    });
+    $http.get(window.location.origin + '/news/comment').then(function (res) {
+        $scope.commentNews = res.data;
+    });
+
+    
+
+    $scope.showAllNews = function (){
+        
+        console.log( $scope.allNews)
+    }
+
+    $scope.selected = 0;
     function changeSelected() {
         $scope.selected++;
         if ($scope.selected == 5) {
             $scope.selected = 0;
         }
-        $timeout(function() {
+        $timeout(function () {
             changeSelected();
         }, 5000);
     }
     changeSelected();
+
+    $scope.selectVisited = 0;
+
+
 });
