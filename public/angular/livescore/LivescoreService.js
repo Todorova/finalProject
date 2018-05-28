24aa5bf4a72f8d828ad7c6b1afdfbc0864d17ce3
Ana Todorova <todorova@outlook.com>
2018-05-28 11:00:43 +0300

    change API key livescore

--- public/angular/livescore/LivescoreService.js
+++ public/angular/livescore/LivescoreService.js
@@ -1,7 +1,7 @@
 app.service('LivescoreService', function ($http) {
 
     this.getLivescore = (from, to) => {
-              return $http.get(`https://apifootball.com/api/?action=get_events&from=${from}&to=${to}&APIkey=bb5f0319a07115c6f2155095cbcd1e9404774f6dd59f4a37d521eb0d2eb9c975&match_live=1`)
+              return $http.get(`https://apifootball.com/api/?action=get_events&from=${from}&to=${to}&APIkey=2791c4a9415e6d589dfba3f0c5f2ade1fa3d4d6dfe84434c25da36998eb86e87&match_live=1`)
     }
     
 });
